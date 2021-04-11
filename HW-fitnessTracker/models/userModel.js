f4f6feb03196f4755056249de59a4d2722e5a67e
Eric Nguyen <erictn3@uci.edu>
2021-04-10 22:55:11 -0700

    first commit

---
+++ HW-fitnessTracker/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// workout schema providing different workout information
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  workouts: [{
+    type: {
+      type: String,
+      trim: true,
+      required: 'Enter a type of exercise',
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: 'Enter an exercise name',
+    },
+    duration: {
+      type: Number,
+      required: 'Enter duration of the exercise',
+    },
+    weight: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    },
+  }] 
+});
+
+const userWorkout = mongoose.model('userWorkout', workoutSchema);
+
+module.exports = userWorkout;
